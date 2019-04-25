7a2ad5a78fb14fbfd29f33a1d7b36db06b5fd7c6
vanschroeder <van@webfreshener.com>
2019-04-25 02:30:30 -0700

    updated deps

--- s3-event.test.js
+++ s3-event.test.js
@@ -1,4 +1,5 @@
 const {S3Event} = require("./index");
+const {TxPipe} = require("txpipe");
 const event = require("./fixtures/s3-put-notification.mock");
 
 describe("S3Event Tests", () => {
@@ -28,5 +29,20 @@ describe("S3Event Tests", () => {
             },
             (e) => done(e),
         ).catch((e) => done(e));
-    })
+    });
+    it("should accept pipes", (done) => {
+        const _event = S3Event(new TxPipe({
+            exec: (d) => {
+                return {statusCode: 200};
+            }
+        }), {exec: (d) => Object.assign(d, _res)});
+        _event(event).then(
+            (res) => {
+                expect(res.statusCode).toEqual(200);
+                expect(res.body).toEqual(_res.body);
+                done();
+            },
+            (e) => done(e),
+        ).catch((e) => done(e));
+    });
 });
